3414304c6853fb993a82fa40d3d530a553ffe52a
Kajman <kaim1396@gmail.com>
2021-05-31 19:36:07 +0200

    Creatre SaveFile Func and upgrade GUI

--- gui.py
+++ gui.py
@@ -8,8 +8,17 @@ import subprocess
 import os
 import sys
 
+#LIST TO FILES
+s="#id x y"
+listSenss = []
+listSenss.append(s)
+SquareArea=160000
+
 
 def InitGui():
+        batteryOn=0
+        batteryOff=0
+        sensorId=0
         main_window =tk.Tk()
         scrollbar = tk.Scrollbar(main_window,orient = 'vertical')
         scrollbar.pack(side='right', fill='y')
@@ -30,29 +39,23 @@ def InitGui():
                 c.pack(side="left")
                 c.create_line(0,400,400,400,fill="black",width=3)
                 c.create_line(0, 0, 0, 400, fill="black", width=7)
-                '''
-                frame1 = tk.Frame(main_window)
-                frame1.pack()
-                MainWindow = c.create_window(10,10,window=frame1, anchor='nw')
-
-                e1 = tk.Entry(frame1)
-                e1.pack()
-'''
 
                 #X and Y
                 # ENTRY
                 count = tk.StringVar()
                 radius = tk.StringVar()
                 color = tk.StringVar()
-                count.set("0")
-                radius.set("0")
-                color.set("0.")
+                count.set("5")
+                radius.set("20")
+                color.set("0.5")
+
                 tk.Label(text="COUNT").pack()
                 countSens = tk.Entry(main_window,textvariable =count, width=10, borderwidth=5, ).pack()
                 tk.Label(text="RADIUS").pack()
                 radiusSens = tk.Entry(main_window,textvariable =radius ,width=10, borderwidth=5).pack()
                 tk.Label(text="% Sensor Activity VALUE: 0-1").pack()
                 propabilityEntry = tk.Entry(main_window, textvariable=color, width=10, borderwidth=5).pack()
+
 
 
 
@@ -116,28 +119,69 @@ def InitGui():
                                 i = i + 20;
 
                 def Init():
-                        colores=0
+                        sensorId=0
+                        listSens= []
+                        SquareArea = 160000.00
+                        SquareTmp=0
                         #List which save param for
-                        CircleList=[]
                         countCIRCLE=int(count.get())
                         colores= float(count.get()) * float(color.get())
+                        # COLOR FOR CIRCLE
                         for colorRed in range (1,1+int(colores)):
+                                sensorId +=1
                                 xval = random.randint(0, 400)
                                 yval = random.randint(0, 400)
                                 s1 = c.create_oval(xval, yval, xval+int(radius.get())*4 , yval+int(radius.get())*4 , stipple="gray50" ,
                                                                              outline="Green", width=1)
+                                s2 = c.create_oval(xval + int(radius.get()) * 4 / 2 + 3,
+                                                        yval + int(radius.get()) * 4 / 2 + 3,
+                                                        xval + int(radius.get()) * 4 / 2,
+                                                        yval + int(radius.get()) * 4 / 2, fill="red",tags=sensorId)
+                                s3 = c.create_text(xval + int(radius.get()) * 4 / 2 + 10,
+                                                   yval + int(radius.get()) * 4 / 2 + 10,
+                                                   font="Times 10 italic bold", text=sensorId)
+                                s=str(sensorId)+" "+ str(xval)+ ".00 " + str(yval)+".00"
+                                listSens.append(s)
+
+                        # COLOR FOR CIRCLE
                         for colorGreen in range(1, 1 + countCIRCLE-int(colores)):
+                                sensorId +=1
                                 xval = random.randint(0, 400)
                                 yval = random.randint(0, 400)
                                 s1 = c.create_oval(xval, yval, xval + int(radius.get())*4, yval + int(radius.get())*4,
                                                    stipple="gray50",
                                                    outline="Red", width=1)
+                                s2 = c.create_oval(xval + int(radius.get())*4/2+3, yval + int(radius.get())*4/2+3,
+                                                        xval + int(radius.get())*4/2, yval + int(radius.get())*4/2,
+                                                        fill="red", tags=sensorId)
+                                #TEXT FOR SENSOR
+                                s3=c.create_text(xval + int(radius.get())*4/2+10, yval + int(radius.get())*4/2+10,
+                                                 font="Times 10 italic bold",text=sensorId)
+                                #TXT FOR READ Value form file
+                                s=str(sensorId)+" "+ str(xval)+ ".00 " + str(yval)+".00"
+                                listSens.append(s)
+                                #COVERAGE
+                                tmp=float(radius.get())*4 * float(radius.get())*4*3.14
+                                SquareTmp+=tmp
+
+                        x=SquareArea-SquareTmp
+                        Coverage=SquareArea-x 
+                        tk.Label(text="% covarage + ").pack()
+                        print(Coverage)
+                        #Write local to global
+                        listSenss.extend(listSens)
+                        #CoverageButton
+                        tk.Label(text="% covarage + ").pack()
+
                 def Exit():
                         python = sys.executable
                         os.execl(python, python, *sys.argv)
 
-                def SaveImage():
-                        x=0
+                def SaveFile():
+                        with open("MySensors .txt", 'w') as file:
+                                for row in listSenss:
+                                        s = "".join(map(str, row))
+                                        file.write(s + '\n')
 
                 def donothing():  # HELPER
                         x = 0
@@ -159,14 +203,19 @@ def InitGui():
                         for x in ListofNumbers:
                                 xx=x[2:4]
                                 yy=x[8:10]
+                                red=random.randint(0,2)
+                                if red==1:
+                                        colo="red"
+                                else:
+                                        colo="green"
                                 print(xx+" "+yy)
-                                c.create_oval(int(xx)*4, int(yy)*4,int(xx)*4+10,int(yy)*4+10, fill="green",stipple="gray50",
-                                                   outline="Red", width=1)
+                                c.create_oval(int(xx)*4, int(yy)*4,int(xx)*4+50,int(yy)*4+50,stipple="gray50",
+                                                   outline=colo, width=1)
 
                 menubar = tk.Menu(main_window)
                 filemenu = tk.Menu(menubar, tearoff=0)
                 filemenu.add_command(label="New", command=donothing)
-                filemenu.add_command(label="Open", command=Open)
+                filemenu.add_command(label="Open FILE: [Test SEnsorId]", command=Open)
                 filemenu.add_command(label="Save", command=donothing)
                 filemenu.add_separator()
                 filemenu.add_command(label="Exit", command=main_window.quit)
@@ -176,7 +225,7 @@ def InitGui():
                 myButton.pack()
                 ExitButton = tk.Button(main_window,text="EXIT",command=Exit)
                 ExitButton.pack(side="right")
-                SaveButton=tk.Button(main_window,text="SaveImage",command=SaveImage)
+                SaveButton=tk.Button(main_window,text="Save File",command=SaveFile)
                 SaveButton.pack(side="left")
 
                 main_window.config(menu=menubar)
