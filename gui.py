bff6cafd9d0ac576ef845fc8e8bb20af186e4749
Kajman <kaim1396@gmail.com>
2021-07-09 13:00:53 +0200

    COverage ALL Q - WORK

--- gui.py
+++ gui.py
@@ -27,7 +27,9 @@ def InitGui():
         ListPOI = []
         IdPOICOV=[]
         SensorHelper=[]
+        SensorHelperPoiID=[]
         ListofNeighbour=[]
+
         tk.Label(text="X 0-100").pack(side="bottom")
         tk.Label(text="Y 0-100").pack(side="left")
         c = tk.Canvas(main_window, width=400, height=400, bg="white")
@@ -405,7 +407,6 @@ def InitGui():
                                     s = "".join(map(str, row))
                                     file.write(s + '\n')
                         SaveFileSenss()
-        Listhelper=[]
         def CalcSingleq():  # calc single q
             r=35
             SensorStates=[]
@@ -498,6 +499,7 @@ def InitGui():
             ys=""
             counter = 1
             ids = 1
+            # OBLICZANIE POKRYCIE KAŻDEGO SENSORA
             for x in converted_listCalcSingleq:
                 helper = 0
                 for y in flaten_list:
@@ -507,7 +509,7 @@ def InitGui():
                         if (ys[0] == '0'):
                             donothing()
                         else:
-                            helper += 1
+                            helper += 1 #VALUE WHEN SENSOR STATE IS 1
                     elif(y[0:3]=='100'):
                         SensorHelper.append(
                             str(circle(int(x[0:2]), int(x[5:7]) ,int(y[0:3]),  int(y[4:]), int(radius.get()), Poir)))
@@ -526,28 +528,24 @@ def InitGui():
                             helper+=1
 
                 coverage=36-helper # ZMINA Z 411
-                IdPOICOV.append(str(ids)+str(coverage)) # ID + AMOUNT OF
+                IdPOICOV.append(str(coverage)) # ID + AMOUNT OF
+                pom=0
                 for x in SensorHelper:
                     ALLPOICOV.append(x)
-
+                    SensorHelperPoiID.append(x+ " - " + str(pom))
+                    pom=pom+1
                # ALLPOICOV.extend(SensorHelper)
                 ids += 1
                 SensorHelper.clear()
-            print(ALLPOICOV)
             ########################################## END POI AMOUNT####################
             chunkser = [ALLPOICOV[x:x + 36] for x in range(0, len(ALLPOICOV), 36)] # ZMIANA Z 411
-            print("CHNKS")
-            for z in chunkser: #CHUNKSER LIST WITH N COVEREGE BY SENSER
-                    ys=str(id) + " " + str(z)
-                    Listhelper.append(ys)
+            chunkserPoi=[SensorHelperPoiID[x:x + 36] for x in range(0, len(SensorHelperPoiID), 36)] # POI in EVERY SENSEOR
             print(chunkser)
-            print("LIST HELPER")
-            print(Listhelper)
             print("POI COV")
             print(ALLPOICOV)
             print("COVEREGE SENSOR BY POI")
             print(IdPOICOV)
-            idstates=1
+            idstates=0
             arubaCloud=[]
             counterchuk=0
             amount=0
@@ -562,22 +560,73 @@ def InitGui():
                         amount = 0
                         for z in y:
                                 if(trucrypt[printerek]==z):
-                                    if(z=='0' and trucrypt != y):
+                                    if(z=='0' ): #and trucrypt != y
                                         amount=amount+1
-                                        arubaCloud.append(printerek)
+                                        arubaCloud.append(str(printerek)) # USUNIECIE PRINTERKA
                                 else:
                                     donothing()
                                 printerek=printerek+1
-                        abc += str(idstates)+ "-" + str(amount) + "\n"
+                        abc += str(idstates)+ "-" + str(amount) + "\n" #AMOUNT + STATES ON
                 counterchuk=counterchuk+1
                 idstates=idstates+1
-            finalStates=dict.fromkeys(arubaCloud)
-            print(list(finalStates))
-            print("Próba")
-            print(amount)
-            print("ABC")
-            print(abc)
+            finalStates=dict.fromkeys(arubaCloud) #DELETE DUPLICATE
+            #COVERAGE Q
+            coverageQ=len(finalStates)/36
+            # COVERAGE EVERY Q
+            qoverageString=0
+            Lista = []
+            strPoi=""
+            for x in chunkserPoi:
+                for y in x:
+                    if(y[0:2]=='-1'):
+                        donothing()
+                    else:
+                        strPoi+=y+"."
+                Lista.append(strPoi) # WSZYSTKIe POI ID z Pokryciem
+                strPoi=""
 
+            def calsum(l):
+                # returning sum of list using List comprehension
+                return sum([int(i) for i in l if type(i) == int or i.isdigit()])
+            # DLA 36 trening
+            ListwihCov=[]
+            asa="0"
+            ListwihCovPercent=[]
+            idCoverage=1
+            for x in Lista:
+                    abc=x.split('.')
+                    helperCov=0
+                    for z in abc:
+                        for y in finalStates:
+                            if(z[4:]==y):
+                                helperCov=helperCov+1
+                    asa= str(helperCov)    #str(helperCov - calsum(ListwihCov))
+                    abc.clear()
+                    idCoverage+=1
+                    #Calculate % POV
+                    ListwihCov.append(asa)
+            # LIST CALCULATION
+            z=[int(x) for x in ListwihCov]
+            x=([int(x) for x in IdPOICOV])
+            products = [a / b for a, b in zip(z, x)]
+            print(products)
+            #CALC SENSOR ID TO TXT
+            ListSensorneigh.append(str(coverageQ) + "  " + "10   " + ' '.join(SensorStates) + "   " + '  '.join(str(round(e,1)) for e in products))
+            '''
+            print("COVERAGE Q")
+            print(ListSensorneigh)
+
+            print("ASA")
+            print(IdPOICOV)
+            print(ListwihCov)
+            print("STR POI")
+            print(strPoi)
+            print(Lista)
+            print("ARUBA")
+            print(arubaCloud)
+            print("PO KONWERSJI")
+            print(list(finalStates))
+            '''
 
 
             def SaveFileSenss():
@@ -590,6 +639,239 @@ def InitGui():
             SaveFileSenss()
                 ###########################################################################################
 
+        def CalcALLq():  # calc single q
+            SensorStates=[]
+            ListPOI.clear()
+            with open('POI36.csv') as file:  # CHANGE TO PO 4412
+                reader=csv.reader(file)
+                for row in reader:
+                    print(row)
+                    ListPOI.append(row)
+            '''
+            ms.showinfo(title=None, message="Read Sensor States")
+            text_fileq = filedialog.askopenfilename(initialdir="C:/", title="Open TextFile",
+                                                    filetypes=(("Text Files", "*.txt"),))
+            state=[]
+            text_fileq = open(text_fileq, 'r')
+            '''
+            state = []
+            text_fileq = open("FILES/sensor-states-10.txt") #do usuniecia potem STEJTY
+            for x in text_fileq:
+                ListofNumbersCalcSingleqState.append(x)
+                state.append(x[12])
+            ListofNumbersCalcSingleqState.pop(0)
+            state.pop(0)
+            print(ListofNumbersCalcSingleqState)
+            print(state)
+            '''
+            ms.showinfo(title=None, message="READ WSN FILE")
+            text_file = filedialog.askopenfilename(initialdir="C:/", title="Open TextFile",
+                                                   filetypes=(("Text Files", "*.txt"),))
+            text_file = open(text_file, 'r')
+            '''
+            text_file = open("FILES/WSN-5d.txt")
+            for x in text_file:
+                ListofNumbersCalcSingleq.append(x)
+                print(ListofNumbersCalcSingleq)
+            ListofNumbersCalcSingleq.pop(0)
+            for x in ListofNumbersCalcSingleq:
+                converted_listCalcSingleq.append(x.strip())
+            for i in range(len(converted_listCalcSingleq)):
+                converted_listCalcSingleq[i]+= state[i]
+            print(converted_listCalcSingleq)
+            ListSensorneigh.clear()
+            # LIST NEIGTBOUR
+            ListSensorneigh.append("#parameters of run: ")
+            ListSensorneigh.append("#Number of Sensors " + "5")
+            ListSensorneigh.append("#Sensor Range: " + str(radius.get()))
+            ListSensorneigh.append("#POI: " + str(variableRadio.get()))
+            ListSensorneigh.append("#Sensor for file: " + str(text_file.name))
+            ListSensorneigh.append("Sensor states for file: not appiled")
+            ListSensorneigh.append("#id q    s    1 2 3 4 5   q1   q2   q3   q4   q5")
+            id = 1
+            '''
+            for x in converted_listCalcSingleq:
+                xx = x[0:2]
+                yy = x[5:7]
+                state=x[9]
+                print(state)
+                if state == '0':
+                    colo = "red"
+                else:
+                    colo = "green"
+                c.create_oval(int(xx) * 4 - 2, int(yy) * 4 - 2, int(xx) * 4 + 2,
+                              int(yy) * 4 + 2, stipple="gray50",
+                              outline="green", fill="green", width=1)
+                c.create_oval(int(xx) * 4 - (int(radius.get()) * 4),
+                              int(yy) * 4 - (int(radius.get()) * 4),
+                              int(xx) * 4 + (int(radius.get()) * 4),
+                              int(yy) * 4 + (int(radius.get()) * 4),
+                              outline=colo, tags=id)
+                c.create_text(int(xx) * 4 + 15, int(yy) * 4 + 15,
+                              font="Times 10 italic bold", text=id)
+                SensorStates.append(state)
+                id += 1;
+                '''
+            print(ListSensorneigh)
+            def circle(x1, y1, x2, y2, r1, r2):
+                distSq = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+                radSumSq = (r1 + r2) * (r1 + r2)
+                if (distSq == radSumSq):
+                    return 1
+                elif (distSq > radSumSq):
+                    return -1
+                else:
+                    return 0
+
+            #POI TO 1d ARRAY
+            flaten_list=reduce(lambda z, y :z + y,ListPOI)
+            ids = 1
+            # OBLICZANIE POKRYCIE KAŻDEGO SENSORA
+            for x in converted_listCalcSingleq:
+                helper = 0
+                for y in flaten_list:
+                    if(y[0]=='0'or y[0:2]=='5;'): #SOLUCJA ZAPISAC CSV JAKO CIĄG STRINGÓW NIE OSOBNĄ LISTE
+                        SensorHelper.append(str(circle(int(x[0:2]),int(x[5:7]), int(y[0]),  int(y[2:]), int(radius.get()),Poir)))
+                        ys=str(circle(int(x[0:2]),int(x[5:7]), int(y[0]),  int(y[2:]), int(radius.get()),Poir))
+                        if (ys[0] == '0'):
+                            donothing()
+                        else:
+                            helper += 1 #VALUE WHEN SENSOR STATE IS 1
+                    elif(y[0:3]=='100'):
+                        SensorHelper.append(
+                            str(circle(int(x[0:2]), int(x[5:7]) ,int(y[0:3]),  int(y[4:]), int(radius.get()), Poir)))
+                        ys=str(circle(int(x[0:2]), int(x[5:7]) ,int(y[0:3]),  int(y[4:]), int(radius.get()), Poir))
+                        if (ys[0] == '0'):
+                            donothing()
+                        else:
+                            helper += 1
+                    else:
+                        SensorHelper.append(
+                            str(circle(int(x[0:2]),  int(x[5:7]), int(y[0:2]), int(y[3:]), int(radius.get()), Poir)))
+                        ys=str(circle(int(x[0:2]),  int(x[5:7]), int(y[0:2]), int(y[3:]), int(radius.get()), Poir))
+                        if(ys[0]=='0'):
+                            donothing()
+                        else:
+                            helper+=1
+
+                coverage=36-helper # ZMINA Z 411
+                IdPOICOV.append(str(coverage)) # ID + AMOUNT OF
+                pom=0
+                for x in SensorHelper:
+                    ALLPOICOV.append(x)
+                    SensorHelperPoiID.append(x+ " - " + str(pom))
+                    pom=pom+1
+               # ALLPOICOV.extend(SensorHelper)
+                ids += 1
+                SensorHelper.clear()
+            ########################################## END POI AMOUNT####################
+            chunkser = [ALLPOICOV[x:x + 36] for x in range(0, len(ALLPOICOV), 36)] # ZMIANA Z 411
+            chunkserPoi=[SensorHelperPoiID[x:x + 36] for x in range(0, len(SensorHelperPoiID), 36)] # POI in EVERY SENSEOR
+            print(chunkser)
+            print("POI COV")
+            print(ALLPOICOV)
+            print("COVEREGE SENSOR BY POI")
+            print(IdPOICOV)
+            idstates=0
+            arubaCloud=[]
+            counterchuk=0
+            amount=0
+            abc=""
+            #FORMAT
+            BinaryList=[]
+            for x in range (0,32):
+                Binary='{:05b}'.format(x)
+                BinaryList.append(Binary)
+            print(BinaryList)
+            for x in BinaryList:
+             for z in x:
+                if(z=='0'):
+                    donothing()
+                else:
+                    trucrypt = chunkser[counterchuk]
+                    for y in chunkser:
+                        printerek=0
+                        amount = 0
+                        for z in y:
+                                if(trucrypt[printerek]==z):
+                                    if(z=='0' ): #and trucrypt != y
+                                        amount=amount+1
+                                        arubaCloud.append(str(printerek)) # USUNIECIE PRINTERKA
+                                else:
+                                    donothing()
+                                printerek=printerek+1
+                        abc += str(idstates)+ "-" + str(amount) + "\n" #AMOUNT + STATES ON
+                counterchuk=counterchuk+1
+                idstates=idstates+1
+            finalStates=dict.fromkeys(arubaCloud) #DELETE DUPLICATE
+            #COVERAGE Q
+            coverageQ=len(finalStates)/36
+            # COVERAGE EVERY Q
+            qoverageString=0
+            Lista = []
+            strPoi=""
+            for x in chunkserPoi:
+                for y in x:
+                    if(y[0:2]=='-1'):
+                        donothing()
+                    else:
+                        strPoi+=y+"."
+                Lista.append(strPoi) # WSZYSTKIe POI ID z Pokryciem
+                strPoi=""
+
+            def calsum(l):
+                # returning sum of list using List comprehension
+                return sum([int(i) for i in l if type(i) == int or i.isdigit()])
+            # DLA 36 trening
+            ListwihCov=[]
+            asa="0"
+            ListwihCovPercent=[]
+            idCoverage=1
+            for x in Lista:
+                    abc=x.split('.')
+                    helperCov=0
+                    for z in abc:
+                        for y in finalStates:
+                            if(z[4:]==y):
+                                helperCov=helperCov+1
+                    asa= str(helperCov)    #str(helperCov - calsum(ListwihCov))
+                    abc.clear()
+                    idCoverage+=1
+                    #Calculate % POV
+                    ListwihCov.append(asa)
+            # LIST CALCULATION
+            z=[int(x) for x in ListwihCov]
+            x=([int(x) for x in IdPOICOV])
+            products = [a / b for a, b in zip(z, x)]
+            print(products)
+            #CALC SENSOR ID TO TXT
+            idAllq=0
+            ListSensorneigh.append(str(idAllq)+ " "+str(coverageQ) + "  " + "10   " + ' '.join(SensorStates) + "   " + '  '.join(str(round(e,1)) for e in products))
+            ListSensorneigh.append("0   0.00  0   0 0 0 0 0   0.00 0.00 0.00 0.00 0.00 ")
+            def SaveFileSenss():
+                with open("creates all-WSN-5.txt"
+                          "", 'w') as file:
+                    for row in ListSensorneigh:
+                        s = "".join(map(str, row))
+                        file.write(s + '\n')
+
+            SaveFileSenss()
+
+            print("COVERAGE Q")
+            print(ListSensorneigh)
+
+            print("ASA")
+            print(IdPOICOV)
+            print(ListwihCov)
+            print("STR POI")
+            print(strPoi)
+            print(Lista)
+            print("ARUBA")
+            print(arubaCloud)
+            print("PO KONWERSJI")
+            print(list(finalStates))
+
+
 
         myButton = tk.Button(main_window, text="SHOW WSN", command=Init)
         myButton.pack()
@@ -601,6 +883,9 @@ def InitGui():
         SaveButton.pack(side="left")
         SaveButton = tk.Button(main_window, text="calc single q", command=CalcSingleq)
         SaveButton.pack(side="left")
+        SaveButton = tk.Button(main_window, text="calc all q", command=CalcALLq)
+        SaveButton.pack(side="left")
+
 #########################################################################################################################
 
         #INSTANCE
